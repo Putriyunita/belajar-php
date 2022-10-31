fb4e9fac335ec0ad5d3be2343ec381df7e353007
Putri Yunita Dwi Ababil <putriyunita590@gmail.com>
2022-10-31 15:13:05 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+echo "Hellow World";
+$nama = "Putri Yunita Dwi Ababil";
+$umur = "21";
+
+echo "Nama saya <strong> $nama</strong>, saya berusia $umur tahun.";
+
+$namaAdik = "Dwi Achmad rehan Syahputra";
+$umurAdik = "13";
+
+echo "Nama Adik Saya <strong> $namaAdik</strong>, Adik saya berusia $umurAdik tahun.<br>";
+
+$selisihUmur = $umur - $umurAdik;
+
+echo "Selisih usia saya dan adik saya adalah $selisihUmur tahun.";
+
+?>
